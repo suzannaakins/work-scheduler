0c674d5182d419978c40b179b9f855bec035cd5e
Garlic Fiend <suzannaakins@gmail.com>
2020-09-01 23:48:18 -0700

    function in js allows user to enter text in form input and stays on page

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,10 @@
 var todayDate = new Date();
 document.getElementById("currentdate").innerHTML = todayDate.toLocaleDateString();
 
+//save users input text input on class "form-control", html element is text area. save to html. when they change it. 
+var createTask = function (taskText) {
+    var task = $(".textarea").text(taskText);
+    //append text to parent element (class=form-group)
+    var input = $(".form-group");
+    input.append(task);
+}
