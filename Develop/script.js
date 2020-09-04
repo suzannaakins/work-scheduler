c822f4c1dbb804afcd6332a3f748ca9e52a1df2a
Garlic Fiend <suzannaakins@gmail.com>
2020-09-03 21:34:44 -0700

    rework color-code function

--- Develop/script.js
+++ Develop/script.js
@@ -8,48 +8,77 @@ var timeblock = document.querySelectorAll('.timeblock');
 // 2. turn node into an array
 var timeBlockArray = Array.from(timeblock);
 //now i want timeBlockArray[0] to have the quality of 9:00AM, [1]=10:00, am, etc
-var k = 9;
-var hour = function () {
-    for (i = 0; i < timeBlockArray.length; i++) {
-        timeBlockArray[i].hour = moment().set("hour", k)
-        k++
-    }
-}
+// var k = 9;
+// var hour = function () {
+//     for (i = 0; i < timeBlockArray.length; i++) {
+//         timeBlockArray[i].hour = moment().set("hour", k)
+//         k++
+//     }
+// }
 
-//if timeBlockArray[[i]].hour is earlier than current time set class. past
-//now, make function so that if current time is equal to timeBlockArray{i}.hour, set class.present
-//if timeBlockArray[i] is later than current time, set class to .future
-var colorCode = function () {
-    if (moment().isAfter(timeBlockArray[i].hour)) {
-        $(".timeblock").addClass("past")
+var update = function() {
+    var currentHour = moment().hours();
+    $(".timeblock").each(function() {
+        var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+    })
+    if (hourBlock < currentHour) {
+        $(this).addClass("past")
     }
-    else if (moment().isSame(timeBlockArray[i].hour)) {
-        $(".timeblock").addClass("present")
+    else if (hourBlock === currentHour) {
+        $(this).removeClass("past")
+        $(this).addClass("present")
     }
-    else if (moment().isBefore(timeBlockArray[i].hour)) {
-        $(".timeblock").addClass("future")
+    else if (hourBlock > currentHour) {
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+        $(this).addClass("future")
+
     }
 }
+ 
+update(); 
+
+// //timeBlock is color code based on past, present, future
+// var colorCode = function (hour) {
+//     console.log(timeBlockArray[1].hour);
+//     if (moment().isAfter(timeBlockArray[i].hour)) {
+//         $(".timeblock").addClass("past")
+//     }
+//     else if (moment().isSame(timeBlockArray[i].hour)) {
+//         $(".timeblock").addClass("present")
+//     }
+//     else if (moment().isBefore(timeBlockArray[i].hour)) {
+//         $(".timeblock").addClass("future")
+//     }
+// }
+
+//form-group clicked to create task
+$(".form-group").on("click", "textarea", function () {
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $("<textarea>")
+        .addClass("time-block")
+        .val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+var saveTask = function () {
+    localStorage.setItem("tasks", JSON.stringify(timeBlockArray));
+};
 
 //save button - on click, will append textarea of input form to the html page, and store to local storage
+$(".saveBtn").on("click", function () {
+    saveTask();
+});
 
-//save users input text input on class "form-control", html element is text area. save to html. when they change it. 
-                    // var createTask = function (taskText) {
-                    //     var task = $(".textarea").text(taskText);
-                    //     //append text to parent element (class=form-group)
-                    //     var input = $(".form-group");
-                    //     input.append(task);
-                    //     console.log(task);
-                    //     //save task to local storage 
-                    //     saveTask();
-                    // }
+console.log(localStorage);
 
+var loadTasks = function () {
+    uploadTasks = JSON.parse(localStorage.getItem("tasks"));
+}
 
-                        // //array for user's to do items
-                        // var task = {};
-
-                        // var saveTask = function () {
-                        //   localStorage.setItem("task", JSON.stringify(task));
-                        // };
-
-//set each ROW to a different time in moments functionality i thiiiink
+loadTasks();
+// hour(); 
+// colorCode();
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Work Day Scheduler Starter Code
+
+This is a website where you can block out your work day by the hour. 
+
+## Description
+
+The main feature of this site is to help you organize your work day - by the hour!  You can enter as many tasks as you'd like into the hour's input form.  Use the save button to save your tasks, so if you refresh your browser, the tasks will reload with it (via your local storage).
+
+Another great feature is that the time blocks are color coded based on where you are in your day - past, present and future.  Your current hour will be highlighted green for you - thus, making it easier fo ryou to see where you "should" be as far as getting things done. 
+
+## How it's built
+
+This application uses Bootstrap to style the timeblocks into rows and columns.  I used Moments and jQuery in Javascript to track where certain timeblock elements on the page were in real time. I loved working with these technologies - they made things easier and quicker than just working purely in Javascript and CSS. 
+
+## Enjoy!
+
+Here is a preview of what you'll be working with:  ![Quiz Start Page](./images/preview.png)
+
+And now, get scheduling! https://suzannaakins.github.io/work-scheduler/
